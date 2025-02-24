dcb803b46446802796c8ae9d8ee297d487054236
KRDen-o <axel.shepherdwilliam@horowhenua.school.nz>
2025-02-25 12:18:46 +1300

    Add files via upload

---
+++ C_01_yes_no_basic.py
@@ -0,0 +1,12 @@
+want_instructions =input("would you like to veiw the intructions?").lower()
+
+if want_instructions == "yes" or want_instructions == "y":
+   print("you said yes")
+
+elif want_instructions == "no" or want_instructions == "n":
+      print ("you said no")
+
+else:
+      print("please enter yes / no")
+
+
